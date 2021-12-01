6496e7d9b04913e78ceb8dc2075b6a70146cf92d
Anna Dimitrakopoulos <dimitrakopoulos.anna@gmail.com>
2021-12-01 09:27:18 +0100

    solved all unit tests

--- src/6-filter-numbers.js
+++ src/6-filter-numbers.js
@@ -1,3 +1,4 @@
 export const filterNumbers = (array, largerThan) => {
-  return array
-}
+  const smallerThan = (value) => value <= largerThan;
+  return array.filter(smallerThan);
+};
--- src/7-is-empty.js
+++ src/7-is-empty.js
@@ -1,9 +1,14 @@
 export const isEmpty = (stringArrayOrObject) => {
-  const type = typeof stringArrayOrObject
+  const type = typeof stringArrayOrObject;
 
-  if (type === 'string') {
-    return stringArrayOrObject === ''
+  if (type === "string") {
+    return stringArrayOrObject === "";
+  } else if (
+    type === "object" &&
+    Object.keys(stringArrayOrObject).length === 0
+  ) {
+    return true;
+  } else {
+    return false;
   }
-
-  return false
-}
+};
